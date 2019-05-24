6b3154b12b3e8d94b507aaddac774fd2c97d1505
Haoran Feng <harrifeng@gmail.com>
2019-05-24 15:47:08 +0800

    Modified at 2019-05-24 15:47:07

--- more-commit.py
+++ more-commit.py
@@ -5,6 +5,6 @@ import random
 for i in range(3):
     ds = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
     os.system('echo {} >> result.txt'.format(ds))
-    time.sleep(2 * random.random())
+    time.sleep(3 * random.random())
     os.system('''git commit -a -m 'Modified at {}' '''.format(ds))
     print("one---------finish---------->>")
