24ad9f90f3809a62548eff91f77acf0c2a6bc5a9
Haoran Feng <harrifeng@gmail.com>
2019-05-24 15:32:33 +0800

    new progress

--- more-commit.py
+++ more-commit.py
@@ -1,6 +1,9 @@
 import os
 import time
-for i in range(10):
-    os.system('''echo `date` >> result.txt''')
+import datetime
+for i in range(3):
+    ds = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+    # os.system({}.format('''echo `date` >> result.txt'''))
+    os.system('echo {} >> result.txt'.format(ds))
     time.sleep(1)
     print("one second later")
