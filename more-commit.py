c5729dc582530b2d0df75865f4d6e6f871929401
Haoran Feng <harrifeng@gmail.com>
2019-05-24 15:32:53 +0800

    go on

--- more-commit.py
+++ more-commit.py
@@ -3,7 +3,6 @@ import time
 import datetime
 for i in range(3):
     ds = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
-    # os.system({}.format('''echo `date` >> result.txt'''))
     os.system('echo {} >> result.txt'.format(ds))
     time.sleep(1)
     print("one second later")
