d92a09ea860e36b52e4f2948bd9580a2ce82cf6b
Haoran Feng <harrifeng@gmail.com>
2019-05-24 16:28:19 +0800

    Feature 006 Modified at 2019-05-24 16:28:18

--- feature-commit.py
+++ feature-commit.py
@@ -4,7 +4,7 @@ import datetime
 import random
 for i in range(5):
     ds = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
-    os.system('echo {} >> result.txt'.format(ds))
+    os.system('echo feature 006 {} >> result.txt'.format(ds))
     time.sleep(random.random())
     os.system('''git commit -a -m 'Feature 006 Modified at {}' '''.format(ds))
     print("one---------finish---------->>")
