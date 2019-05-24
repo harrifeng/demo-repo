7e036eb6321f51446ff6d90617dc0c94cc17768f
Haoran Feng <harrifeng@gmail.com>
2019-05-24 15:46:30 +0800

    Modified at 2019-05-24 15:46:28

--- more-commit.py
+++ more-commit.py
@@ -1,9 +1,10 @@
 import os
 import time
 import datetime
+import random
 for i in range(3):
     ds = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
     os.system('echo {} >> result.txt'.format(ds))
-    time.sleep(1)
+    time.sleep(2 * random.random())
     os.system('''git commit -a -m 'Modified at {}' '''.format(ds))
     print("one second later")
