14c694de956dcd6a59c301d1dd77add0cd78715a
Haoran Feng <harrifeng@gmail.com>
2019-05-24 15:55:18 +0800

    Feature 001 Modified at 2019-05-24 15:55:18

--- more-commit.py
+++ more-commit.py
@@ -6,5 +6,5 @@ for i in range(10):
     ds = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
     os.system('echo {} >> result.txt'.format(ds))
     time.sleep(random.random())
-    os.system('''git commit -a -m 'Modified at {}' '''.format(ds))
+    os.system('''git commit -a -m 'Feature 001 Modified at {}' '''.format(ds))
     print("one---------finish---------->>")
