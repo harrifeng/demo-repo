91d203a3c49fc376946b2eb8f33933acf0a4dc62
Haoran Feng <harrifeng@gmail.com>
2019-05-24 16:32:26 +0800

    Modified at 2019-05-24 16:32:25

--- more-commit.py
+++ more-commit.py
@@ -4,6 +4,7 @@ import datetime
 import random
 for i in range(10):
     ds = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+    os.system('git add .')
     os.system('echo {} >> result-{}.txt'.format(ds, i))
     time.sleep(random.random())
     os.system('''git commit -a -m 'Modified at {}' '''.format(ds))
